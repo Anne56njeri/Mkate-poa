9aad727cf70f2b8811775b21ce8c50a98dc42e30
MaryAnne45 <mary.jereh@gmail.com>
2018-10-15 11:39:54 +0300

    added a css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
-
+import "./index.css";
+// this is the entry point of the app
 ReactDOM.render(
   <Main/>,
   document.getElementById("root")
