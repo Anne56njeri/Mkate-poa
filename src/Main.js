cfe052e1442ec49f36f6415b4baee5c976aa2239
MaryAnne45 <mary.jereh@gmail.com>
2018-10-15 11:36:42 +0300

    added routes to the home page the contact page and the service page

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,32 @@
 import React ,{ Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Services from "./Services";
+import Contact from "./Contact";
 
+// this is the app frame , the parent Component
 class Main extends Component {
   render() {
     return (
+      <HashRouter>
       <div>
       <h1>Mkate poa website</h1>
       <ul className="header">
-      <li><a href="/">Home</a></li>
-      <li><a href="/services">Services</a></li>
-      <li><a href="/contact">Contact</a></li>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/services">Services</NavLink></li>
+      <li><NavLink to="/contact">Contact</NavLink></li>
       </ul>
       <div className="content">
+      <Route path="/" component={Home}/>
+      <Route path="/services" component={Services}/>
+      <Route path="/Contact" component={Contact}/>
       </div>
       </div>
+      </HashRouter>
     );
   }
 }
