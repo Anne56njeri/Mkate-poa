e8b881943a2a60478aa97d519343a54bcb93fcaf
MaryAnne45 <mary.jereh@gmail.com>
2018-10-15 11:22:55 +0300

    added a service component

---
+++ src/service.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Services extends Component {
+  render() {
+    return (
+      <div>
+      <h2>Services</h2>
+      <p>Mauris sem velit, vehicula eget sodales vitae,
+        rhoncus eget sapien</p>
+        </div>
+    );
+  }
+}
+export default Services;
